405b0cbbab1d17c5a7d6f2f2b4c7f81a75a015bc
duanApple <duanapple@duanappledemac.local>
2016-10-26 20:40:51 +0800

    项目部署

---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by duanApple on 16/10/26.
+//  Copyright © 2016年 Wuhan University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
